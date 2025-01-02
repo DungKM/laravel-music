d9c4c1159caed359ed87be699457fbe67995dc0d
DungKM <hoanganhdung13122000@gmail.com>
2025-01-02 18:08:59 +0700

    feat: install boostrapt 5

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,7 @@ import { createApp } from "vue";
 import router from './router/index.js'
 import App from "./App.vue";
 
+import 'bootstrap/dist/css/bootstrap-grid.min.css';
 const app = createApp(App);
 app.use(router);
 app.mount("#app");
