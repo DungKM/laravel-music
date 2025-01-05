23da06d0c6e7573f6b7236c533b924621bb1d09d
DungKM <hoanganhdung13122000@gmail.com>
2025-01-06 00:57:09 +0700

    feat: layout admin mobi

---
+++ resources/js/components/TheHeader.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container-fluid">
+    <div class="row text-white" style="background-color: #0c713d; padding: 1rem">
+      <div class="col-1 d-flex d-sm-none align-items-center justify-content-center">
+        <span @click="showDrawer">X</span>
+      </div>
+      <div class="col-10 col-sm-9 d-flex align-items-center justify-content-center justify-content-sm-start">
+        <img src="https://tse2.mm.bing.net/th?id=OIP.XrGVljajcLZhvJGUD-Sc7gHaE7&pid=Api&P=0&h=180" alt="Logo"
+          style="width: 50px; height: 50px">
+        <span class="d-none d-sm-flex">Quản trị</span>
+      </div>
+      <div class="col-sm-3 d-none d-sm-flex align-items-center justify-content-sm-end">
+        <span>Admin</span>
+      </div>
+      <div class="col-1 d-flex d-sm-none align-items-center justify-content-center">
+        <span @click="showDrawerUser">X</span>
+      </div>
+    </div>
+  </div>
+  <a-drawer v-model:open="open" class="custom-class" root-class-name="root-class-name" :root-style="{ color: 'blue' }"
+    style="color: red" title="Danh mục" placement="right" @after-open-change="afterOpenChange">
+    <p>Some contents...</p>
+    <p>Some contents...</p>
+    <p>Some contents...</p>
+  </a-drawer>
+  <a-drawer v-model:open="open_user" class="custom-class" root-class-name="root-class-name" :root-style="{ color: 'blue' }"
+    style="color: red" title="Admin" placement="right" @after-open-change="afterOpenChange">
+    <p>Some contents...</p>
+    <p>Some contents...</p>
+    <p>Some contents...</p>
+  </a-drawer>
+</template>
+<script setup>
+import { ref } from 'vue';
+const open = ref(false);
+const open_user = ref(false);
+const afterOpenChange = bool => {
+  console.log('open', bool);
+};
+const showDrawer = () => {
+  open.value = true;
+};
+const showDrawerUser = () => {
+  open_user.value = true;
+};
+</script>
--- resources/js/layouts/admin.vue
+++ resources/js/layouts/admin.vue
@@ -1,6 +1,22 @@
 <template>
-    Layouts Admin
-    <router-view>
-        
-    </router-view>
+    <TheHeader />
+    <div class="container-fuild">
+        <div class="row">
+            <div class="col-sm-3 d-none d-sm-flex">
+                side bar
+            </div>
+            <div class="col-sm-12">
+                <router-view></router-view>
+            </div>
+        </div>
+    </div>
 </template>
+<script>
+import TheHeader from '../components/TheHeader.vue';
+
+export default {
+    components: {
+        TheHeader
+    }
+}
+</script>
